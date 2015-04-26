0f7d40431bc0333fc6cae02c3189e97d15d41a5a
juanpo <juanpo1987@gmail.com>
2015-04-25 21:22:49 -0430

    redirect al url

--- compras/urls.py
+++ compras/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'compras.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', RedirectView.as_view(url='/productos/')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^productos/', include('compras_app.urls')),
 )

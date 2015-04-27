40a6a0b6f4cc7c8c990b38e3a933704663ccd701
juanpo <juanpo1987@gmail.com>
2015-04-26 23:27:19 -0430

    agregue views nuevos

--- compras_app/views.py
+++ compras_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from models import Producto
 from django.views import generic
+from django.views.generic.list import MultipleObjectMixin
 
 # Create your views here.
 
@@ -10,22 +11,25 @@ class ProductosView(generic.ListView):
     model = Producto
     template_name = 'compras_app/index.html'
     context_object_name = 'productos'
-    #queryset = Producto.objects.order_by("descripcion")
-
+    paginate_by = 100
+    
 class DescripcionAscView(generic.ListView):
 	template_name = "compras_app/productos.html"
 	context_object_name = "productos"
+	paginate_by = 100
 	queryset = Producto.objects.order_by("descripcion")
 
 class DescripcionDescView(generic.ListView):
 	template_name = "compras_app/productos.html"
 	context_object_name = "productos"
 	queryset = Producto.objects.order_by("-descripcion")
+	paginate_by = 100
 
 class CodigoDescView(generic.ListView):
 	template_name = "compras_app/productos.html"
 	context_object_name = "productos"
 	queryset = Producto.objects.order_by("-codigo")
+	paginate_by = 100
 
 
 def detail(request, codigo):

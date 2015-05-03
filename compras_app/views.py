71bd8526a7776e81c8637820e146c79df41d6af5
juanpo <juanpo1987@gmail.com>
2015-05-03 18:27:22 -0430

    converti el search en un listview

--- compras_app/views.py
+++ compras_app/views.py
@@ -1,48 +1,52 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from models import Producto
-from django.views import generic
+from django.views.generic import ListView
 from braces.views import OrderableListMixin
 from compras_app.utils import get_query
 
 # Create your views here.
 
 
-class ProductosView(OrderableListMixin, generic.ListView):
+class ProductosView(OrderableListMixin, ListView):
     model = Producto
     template_name = 'compras_app/productos.html'
     context_object_name = 'productos'
     paginate_by = 100
     orderable_columns = (u"codigo", u"descripcion",)
     orderable_columns_default = u"descripcion"
-    
+
+class SearchView(ListView):
+    model = Producto
+    template_name = "compras_app/search_results.html"
+    paginate_by = 50
+    context_object_name = "found_entries"
+
+    def get_queryset(self):
+        found_entries = ""
+        queryset = super(SearchView, self).get_queryset()
+
+        if ('q' in self.request.GET) and self.request.GET['q'].strip():
+        
+            query_string = self.request.GET['q']
+            entry_query = get_query(query_string, ['descripcion', 'codigo'])
+            found_entries = Producto.objects.filter(entry_query).order_by("descripcion")
+
+        if ("ordering" in self.request.GET) and ("order_by" in self.request.GET):
+        #ya estando los dos, se desarrolla la logica.
+            campo = self.request.GET["order_by"]
+            sentido = self.request.GET["ordering"]
+            #se le asigna el valor a cada una
+
+            if (sentido == "asc"):
+                found_entries = Producto.objects.filter(entry_query).order_by(campo)
+
+            else:
+                found_entries = Producto.objects.filter(entry_query).order_by("-" + campo)
+
+        queryset = found_entries
+        return queryset
+
 def detail(request, codigo):
     descripcion = get_object_or_404(Producto, codigo = codigo)
     return render(request, "compras_app/detail.html", {"codigo": codigo, "descripcion": descripcion})
-
-def search(request):
-    query_string = ''
-    found_entries = None
-    
-    if ('q' in request.GET) and request.GET['q'].strip():
-        
-        query_string = request.GET['q']
-        entry_query = get_query(query_string, ['descripcion', 'codigo'])
-        found_entries = Producto.objects.filter(entry_query).order_by('descripcion')
-
-    if ("ordering" in request.GET) and ("order_by" in request.GET):
-        #ya estando los dos, se desarrolla la logica.
-        campo = request.GET["order_by"]
-        sentido = request.GET["ordering"]
-        #se le asigna el valor a cada una
-
-        if (sentido == "asc"):
-            found_entries = Producto.objects.filter(entry_query).order_by(campo)
-
-        else:
-            found_entries = Producto.objects.filter(entry_query).order_by("-" + campo)
-
-    return render(request, 'compras_app/search_results.html', {
-		'query_string': query_string,
-		'found_entries': found_entries,
-		})

16240276b6ca98a1c6bc6dd20b2aed637c42d72f
juanpo <juanpo1987@gmail.com>
2015-05-04 18:22:13 -0430

    modifique pequeño bug en el paginator

--- compras_app/urls.py
+++ compras_app/urls.py
@@ -7,5 +7,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.ProductosView.as_view(), name='productos'),
     url(r'^(?P<codigo>[A-Z0-9]+)/$', views.detail, name='detail'),
-    url(r'^search/$', views.SearchView.as_view(), name='search'),
+    url(r'^search$', views.SearchView.as_view(), name='search'),
 ]
